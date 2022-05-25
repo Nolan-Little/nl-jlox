3b533fb33d4714a95e8afb8a094f337cbcb55696
Nolan-Little <little.nolan@gmail.com>
2022-05-25 11:40:42 -0400

    compete 8.3

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -5,6 +5,8 @@ import java.util.List;
 class Interpreter implements Expr.Visitor<Object>,
                              Stmt.Visitor<Void> {
 
+  private Environment environment = new Environment();
+                              
   void interpret(List<Stmt> statements) {
     try {
       for (Stmt statement : statements) {
@@ -34,6 +36,11 @@ class Interpreter implements Expr.Visitor<Object>,
 
     // Unreachable.
     return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
   }
 
   private void checkNumberOperand(Token operator, Object operand) {
@@ -99,6 +106,17 @@ class Interpreter implements Expr.Visitor<Object>,
     return null;
   }
   
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     Object left = evaluate(expr.left);
